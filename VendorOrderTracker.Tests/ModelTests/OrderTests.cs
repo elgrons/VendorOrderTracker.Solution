664a8e783b9f8b3b0b8de7315d7503bb4399a0c4
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-03 10:18:35 -0800

    second test passes for Order get title.

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -12,8 +12,20 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test", "test", 1, 2);
+      Order newOrder = new Order("test", "test", 1, "Thursday");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnTitle_String()
+    {
+      string title = "Test";
+      string description = "Bread";
+      int price = 100;
+      string date = "Thursday";
+      Order newOrder = new Order(title, description, price, date);
+      string result = newOrder.Title;
+      Assert.AreEqual(title, result);
     }
 
   }
