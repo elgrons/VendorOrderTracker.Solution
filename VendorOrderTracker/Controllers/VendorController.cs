11224e90a17045c8ede8803c66a2839099ba558a
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-03 15:25:09 -0800

    update New.cshtml Orders view with functional form.

--- VendorOrderTracker/Controllers/VendorController.cs
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -5,7 +5,7 @@ using VendorOrderTracker.Models;
 
 namespace VendorOrderTracker.Controllers
 {
-  public class VendorController : Controller
+  public class VendorsController : Controller
   {
     [HttpGet("/vendors")]
     public ActionResult Index()
