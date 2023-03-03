a5215ae4ef9e807fdb3c4ee356d8e12944e73175
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-03 11:40:18 -0800

    WIP: VendorController to use form to add new Vendor.

--- VendorOrderTracker/Controllers/OrderController.cs
+++ VendorOrderTracker/Controllers/OrderController.cs
@@ -7,6 +7,32 @@ namespace VendorOrderTracker.Controllers
 {
   public class OrderController : Controller
   {
+    [HttpGet("/orders/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/orders")]
+    public ActionResult Create(string title, string description, int price, string date)
+    {
+      Order myOrder = new Order(title, description, price, date);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpGet("/orders/{id}")]
+    public ActionResult Show(int id)
+    {
+      Order foundOrder = Order.Find(id);
+      return View(foundOrder);
+    }
+
 
   }
 }
--- VendorOrderTracker/Controllers/VendorController.cs
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -7,13 +7,36 @@ namespace VendorOrderTracker.Controllers
 {
   public class VendorController : Controller
   {
-    [HttpGet("/vendor")]
+    [HttpGet("/vendors")]
     public ActionResult Index()
     {
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
 
-    
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      CD selectedCD = CD.Find(id);
+      List<Track> tracks = selectedCD.Tracks;
+      model.Add("cd", selectedCD);
+      model.Add("tracks", tracks);
+      return View(model);
+    }
+
   }
 }
